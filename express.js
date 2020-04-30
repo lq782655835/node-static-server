29752892cd5054067ed76f7b12a448563a55fec3
springleo <782655835@qq.com>
2020-04-30 13:41:08 +0800

    fix: fix express

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-app.use('static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 
 http.createServer(app).listen('8000', function() {
 	console.log('open the browers and enter http://localhost:8000/test.html')
